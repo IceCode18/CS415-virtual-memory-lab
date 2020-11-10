d5721a1651340216e4127b7df23b89098a3defd5
Kaiser19 <jtiano19@go.byuh.edu>
2020-11-09 20:52:43 -1000

    Add getFreeFrame method to Memory class

--- Memory.java
+++ Memory.java
@@ -18,7 +18,20 @@ public class Memory {
         insertedFrames = 0;
     }
 
-
+    /**
+     * Set a frame in memory using a specific entry
+     * @param e PageTableEntry type object to set memory frame to
+     */
+    public void getFreeFrame(PageTableEntry e){
+        if( insertedFrames < maxFrames){
+            memory.add(e);
+        }else{
+            int newIndex = (insertedFrames % maxFrames); // mod algorithm to enable round-robin frame setting style
+            memory.get(newIndex).kickFromMemory(); // update inMemory boolean variable to false
+            memory.set(newIndex,e); // update memory frame with a page table entry
+        }
+        insertedFrames++;
+    }
 
 }
 
