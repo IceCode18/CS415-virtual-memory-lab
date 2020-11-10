e7cc0ebb6e825b4d5a38e32c5e64eb8fb876299a
Kaiser19 <jtiano19@go.byuh.edu>
2020-11-09 20:25:53 -1000

    Add newProcess method to OS class

--- OperatingSystem.java
+++ OperatingSystem.java
@@ -2,12 +2,25 @@ package com.company;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class OperatingSystem {
 
-    public OperatingSystem(){
+    ArrayList<PageTable> processTable;
 
+    private int currentProcessIndex;
+    private int access, miss, compulsoryMiss, hit;
+    private final int MAX_ENTRIES = 64;
+    private final int MAX_MEMORY_FRAMES = 30;
+
+    public OperatingSystem(){
+        currentProcessIndex = -1;
+        access = 0;
+        miss = 0;
+        compulsoryMiss = 0;
+        hit = 0;
+        processTable = new ArrayList<>();
     }
 
     protected void start(){
@@ -30,5 +43,11 @@ public class OperatingSystem {
             System.out.println("Cannot find file!");
         }
     }
+
+    private void newProcess(){
+        PageTable t = new PageTable(MAX_ENTRIES);
+        processTable.add(t);
+    }
+
 
 }
